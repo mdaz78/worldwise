91ce39a2c8ca8a4f2c7e566635c7481566e34e01
mdaz78 <mdabuzafar78@gmail.com>
2025-12-22 13:11:46 +0530

    feat: update AppNav to include navigation links for Cities and Countries

--- src/components/AppNav/index.jsx
+++ src/components/AppNav/index.jsx
@@ -1,7 +1,19 @@
+import { NavLink } from 'react-router-dom';
 import styles from './index.module.css';
 
 const AppNav = () => {
-  return <nav className={styles.nav}>App Nav</nav>;
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to='cities'>Cities</NavLink>
+        </li>
+        <li>
+          <NavLink to='countries'>Countries</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default AppNav;
