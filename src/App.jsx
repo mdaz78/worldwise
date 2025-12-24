7fd4c924309a7c4db4dcffa0e79409d343d20478
mdaz78 <mdabuzafar78@gmail.com>
2025-12-24 20:29:19 +0530

    refactor: implement lazy loading for routes in App component and enhance loading experience with SpinnerFullPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
+import { AuthProvider } from './contexts/AuthContext';
+import CitiesProvider from './contexts/CitiesContext';
+
+import { Suspense, lazy } from 'react';
 import City from './components/City';
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
 import Form from './components/Form';
-import { AuthProvider } from './contexts/AuthContext';
-import CitiesProvider from './contexts/CitiesContext';
-import AppLayout from './pages/AppLayout';
-import Homepage from './pages/Homepage';
-import Login from './pages/Login';
-import PageNotFound from './pages/PageNotFound';
-import Pricing from './pages/Pricing';
-import Product from './pages/Product';
+
+// import AppLayout from './pages/AppLayout';
+// import Homepage from './pages/Homepage';
+// import Login from './pages/Login';
+// import PageNotFound from './pages/PageNotFound';
+// import Pricing from './pages/Pricing';
+// import Product from './pages/Product';
+
+import SpinnerFullPage from './components/SpinnerFullPage';
 import ProtectedRoute from './pages/ProtectedRoute';
+
+const Homepage = lazy(() => import('./pages/Homepage'));
+const AppLayout = lazy(() => import('./pages/AppLayout'));
+const Login = lazy(() => import('./pages/Login'));
+const PageNotFound = lazy(() => import('./pages/PageNotFound'));
+const Pricing = lazy(() => import('./pages/Pricing'));
+const Product = lazy(() => import('./pages/Product'));
 
 function App() {
   return (
@@ -19,27 +32,29 @@ function App() {
       <CitiesProvider>
         <div>
           <BrowserRouter>
-            <Routes>
-              <Route index element={<Homepage />} />
-              <Route path='product' element={<Product />} />
-              <Route path='pricing' element={<Pricing />} />
-              <Route path='login' element={<Login />} />
-              <Route
-                path='app'
-                element={
-                  <ProtectedRoute>
-                    <AppLayout />
-                  </ProtectedRoute>
-                }
-              >
-                <Route index element={<Navigate to='cities' replace />} />
-                <Route path='cities' element={<CityList />} />
-                <Route path='cities/:id' element={<City />} />
-                <Route path='countries' element={<CountryList />} />
-                <Route path='form' element={<Form />} />
-              </Route>
-              <Route path='*' element={<PageNotFound />} />
-            </Routes>
+            <Suspense fallback={<SpinnerFullPage />}>
+              <Routes>
+                <Route index element={<Homepage />} />
+                <Route path='product' element={<Product />} />
+                <Route path='pricing' element={<Pricing />} />
+                <Route path='login' element={<Login />} />
+                <Route
+                  path='app'
+                  element={
+                    <ProtectedRoute>
+                      <AppLayout />
+                    </ProtectedRoute>
+                  }
+                >
+                  <Route index element={<Navigate to='cities' replace />} />
+                  <Route path='cities' element={<CityList />} />
+                  <Route path='cities/:id' element={<City />} />
+                  <Route path='countries' element={<CountryList />} />
+                  <Route path='form' element={<Form />} />
+                </Route>
+                <Route path='*' element={<PageNotFound />} />
+              </Routes>
+            </Suspense>
           </BrowserRouter>
         </div>
       </CitiesProvider>
--- src/components/SpinnerFullPage/index.jsx
+++ src/components/SpinnerFullPage/index.jsx
@@ -1,5 +1,5 @@
-import Spinner from "./Spinner";
-import styles from "./SpinnerFullPage.module.css";
+import Spinner from '../Spinner';
+import styles from './index.module.css';
 
 function SpinnerFullPage() {
   return (
