c02ac0d25b5e6fbf2c2da78896345564e83df935
mdaz78 <mdabuzafar78@gmail.com>
2025-12-24 12:46:19 +0530

    chore: remove log statements

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,11 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from '../../components/Button';
 import PageNav from '../../components/PageNav';
+import { useAuth } from '../../contexts/AuthContext';
 import styles from './index.module.css';
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState('jack@example.com');
   const [password, setPassword] = useState('qwerty');
+  const { login, isAuthenticated } = useAuth();
+
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    login(email, password);
+  };
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/app/cities', { replace: true });
+    }
+  }, [isAuthenticated, navigate]);
 
   return (
     <main className={styles.login}>
@@ -32,7 +50,9 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type='primary' onClick={handleLogin}>
+            Login
+          </Button>
         </div>
       </form>
     </main>
